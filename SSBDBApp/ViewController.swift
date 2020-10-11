88ffe8e97b98560362509eae409842c638bcce38
Ninja-Lombax <godzillakotm80@gmail.com>
2020-10-11 17:51:48 -0500

    Initial commit

--- SSBDBApp/ViewController.swift
+++ SSBDBApp/ViewController.swift
@@ -7,14 +7,99 @@
 //
 
 import UIKit
+import Foundation
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    var dataManager : NSManagedObjectContext!
+    var listArray = [NSManagedObject]()
+    @IBOutlet var txtEnterCharacter: UITextField!
+    
+    
+    @IBOutlet var txtCharLisy: UITextView!
+    
+    @IBOutlet var txtdeleteChar: UITextField!
+    
+    
+    @IBAction func addChar(_ sender: UIButton) {
+        
+         let newEntity = NSEntityDescription.insertNewObject(forEntityName: "SuperSmashBros", into: dataManager)
+        
+        newEntity.setValue(txtEnterCharacter.text!, forKey: "character")
+        
+        do{
+            try self.dataManager.save()
+            listArray.append(newEntity)
+        }catch{
+            print("Error saving data")
+            
+            
+        }
+        txtEnterCharacter.text?.removeAll()
+        txtdeleteChar.text?.removeAll()
+        txtCharLisy.text?.removeAll()
+        
+        retrieveData()
+        
+    }
+    
+    
+    
+    @IBAction func removeChar(_ sender: UIButton) {
+        let deleteItem = txtdeleteChar.text!
+        for item in listArray
+        {
+            if item.value(forKey: "character") as! String == deleteItem{
+            dataManager.delete(item)
+            }
+        }
+        
+        do{
+            try self.dataManager.save()
+        }
+        catch{
+            
+            print("Error deleting data")
+        }
+        
+        txtEnterCharacter.text?.removeAll()
+        txtdeleteChar.text?.removeAll()
+        txtCharLisy.text?.removeAll()
+        retrieveData()
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        dataManager = appDelegate.persistentContainer.viewContext
+        txtCharLisy.text?.removeAll()
+        retrieveData()        // Do any additional setup after loading the view.
     }
 
-
+    func retrieveData()
+     {
+         let fetchRequest : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "SuperSmashBros")
+         do{
+             let result = try dataManager.fetch(fetchRequest)
+             listArray = result as! [NSManagedObject]
+             
+             for item in listArray
+             {
+                 let product = item.value(forKey: "character") as! String
+                 txtCharLisy.text! += product
+                txtCharLisy.text! += "\n"
+                 
+             }
+             
+         } catch{
+             print("Error retrieving data")
+         }
+         
+     }
+    
 }
 
